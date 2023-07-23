936fb1205be3180269eede84e5f38aef86e0a41f
coolgreatgamer <96445003+coolgreatgamer@users.noreply.github.com>
2023-07-23 09:59:52 -0700

    made movement
    the keys work, but the right key only works once💀

--- game2/code.js
+++ game2/code.js
@@ -25,20 +25,33 @@ function preload() {
 }
 
 
-function playerControls() {
-    if (keyIsDown(RIGHT_ARROW)) {
-      player.position.x += speed; 
-     } 
-     if (keyIsDown(LEFT_ARROW)) {
-      player.position.x -= speed; 
-      } 
-      if (keyIsDown(DOWN_ARROW)) { 
-        player.position.y += speed;
-      } 
-      if (keyIsDown(UP_ARROW)) {
-        player.position.y -= speed;
-       }
-  if (kb.presses('space')) {
-	background(0, 255, 0);
-  }
+function keyPressed() {
+    //if (keyIsDown(RIGHT_ARROW)) {
+    //  player.position.x += speed; 
+    // } 
+    // if (keyIsDown(LEFT_ARROW)) {
+    //  player.position.x -= speed; 
+    //  } 
+    //  if (keyIsDown(DOWN_ARROW)) { 
+    //    player.position.y += speed;
+    //  } 
+    //  if (keyIsDown(UP_ARROW)) {
+    //    player.position.y -= speed;
+    //   }
+
+    if (keyCode == RIGHT_ARROW) {
+      player.setSpeed(1.5, 0);
+    }
+    else if (keyCode == DOWN_ARROW) {
+      player.setSpeed(1.5, 90);
+    }
+    else if (keyCode == LEFT_ARROW) {
+      player.setSpeed(1.5, 180);
+    }
+    else if (keyCode == UP_ARROW) {
+      player.setSpeed(1.5, 270);
+    }
+    else if (key == ' ') {
+      player.setSpeed(0, 0);
+    }
 }

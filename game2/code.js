92e711898eebf26b817e2a55cb3669d172c0f384
coolgreatgamer <96445003+coolgreatgamer@users.noreply.github.com>
2023-07-25 18:03:52 -0700

    made a enemy and added a border so you don't go outside of the map
    (the enemy has no ai yet :( )

--- game2/code.js
+++ game2/code.js
@@ -5,7 +5,10 @@ let playerY =32;
 let floor
 let group;
 
-let speed = 10;
+let enemyImg;
+let enemy;
+
+let speed = 5;
 
 function setup() {
   world.gravity.y = 5;
@@ -21,32 +24,35 @@ function setup() {
   //group = new group()
 	player = new Sprite(width/2, height/2, playerX, playerY);
   player.addImage(playerImg);
+  enemy = new Sprite(width/2, height/2, 50, 50);
+  enemy.addImage(playerImg);
 }
 
 function draw() {
   background("black");
   drawSprites();
-
   if (kb.pressing(RIGHT_ARROW)) {
-    world.gravity.y = 5;
     player.position.x += speed;
-    world.gravity.y = 5;
+    if (player.position.x > 600) {
+      player.position.x = 600;
+    }
   } 
   if (kb.pressing(LEFT_ARROW)) {
-    world.gravity.y = 5;
     player.position.x -= speed;
-    world.gravity.y = 5;
+    if (player.position.x < 0) {
+      player.position.x = 0;
+    }
+
   } 
   if (kb.pressing(DOWN_ARROW)) { 
-    world.gravity.y = 5;
-    //player.position.y += speed;
-    world.gravity.y = 5;
+    player.position.y += speed;
+     //disabeled due to the type of game.
   } 
-  //disabeled due to the type of game.
   if (kb.pressing(UP_ARROW)) {
-    world.gravity.y = 5;
     player.position.y -= speed;
-    world.gravity.y = 5;
+    if (player.position.y < 0) {
+      player.position.y = 0;
+    }
   }
   
 }
@@ -69,58 +75,3 @@ function keyPressed() {
 
 
 
-
-
-
-
-
-
-
-
-
-const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
-function drawPlayer() {
-    ctx.fillStyle = 'blue';
-    ctx.fillRect(player.x, player.y, player.width, player.height);
-}
-
-function update() {
-    // Check for input and move the player
-    if (keys.ArrowUp) {
-        player.y -= player.speed;
-    }
-
-    // Check if the player touches the roof
-    if (player.y <= 0) {
-        player.isTouchingRoof = true;
-    }
-
-    // Clear the canvas and draw the player
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawPlayer();
-
-    // Check if the player has won
-    if (player.isTouchingRoof) {
-        ctx.font = '30px Arial';
-        ctx.fillStyle = 'green';
-        ctx.fillText('You Win!', canvas.width / 2 - 50, canvas.height / 2);
-        return;
-    }
-
-    // Request the next animation frame
-    requestAnimationFrame(update);
-}
-
-// Event listeners for input handling
-const keys = {};
-window.addEventListener('keydown', (event) => {
-    keys[event.key] = true;
-});
-
-window.addEventListener('keyup', (event) => {
-    keys[event.key] = false;
-});
-
-// Start the game loop
-update();

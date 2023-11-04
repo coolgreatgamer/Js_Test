aca82d503048defb60538f4372d001641eee4252
coolgreatgamer <96445003+coolgreatgamer@users.noreply.github.com>
2023-11-03 19:18:56 -0700

    change canvas and background color to dark

--- dad1/sketch.js
+++ dad1/sketch.js
@@ -1,23 +1,30 @@
 let player, floor;
 
 function setup() {
-	new Canvas(700, 700);
-	world.gravity.y = 10;
-	player = new Sprite();
-	player.scale = 2;
-	player.img = 'mario.png';
+	new Canvas(500, 500);
+
+	
+	//world.gravity.y = 10;
+
+	//player = new Sprite();
+	//player.scale = 2;
+	//player.img = 'mario.png';
+	
 	//player.debug = mouse.pressing();
 	//                   X    Y
-	floor = new Sprite(250, 600, 10000, 40, 'static');
+	//floor = new Sprite(250, 600, 10000, 40, 'static');
 
 }
 
 function draw() {
 	clear();
+
+	background(51);
+	
 	if (kb.presses('space')) {
-		player.vel.y = -4;
-		player.vel.x = 3;
+		//player.vel.y = -4;
+		//player.vel.x = 3;
 	}
 
-	camera.x = player.x;
+	//camera.x = player.x;
 }
